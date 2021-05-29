f6687eaa0739f0b9aa4aa3944a7227a7ee52d718
danibondar <danibondar@gmail.com>
2021-05-29 10:28:18 +0300

    implementation of the heuristic functions - monotonic_board needs more work, but got to 1024 on an avg case

--- submission.py
+++ submission.py
@@ -1,6 +1,7 @@
 import logic
 import random
 from AbstractPlayers import *
+from math import log
 import time
 
 # commands to use for move players. dictionary : Move(enum) -> function(board),
@@ -8,6 +9,8 @@ import time
 # new_board is according to the step taken, done is true if the step is legal, score is the sum of all numbers that
 # combined in this step.
 # (you can see GreedyMovePlayer implementation for example)
+from constants import *
+
 commands = {Move.UP: logic.up, Move.DOWN: logic.down,
             Move.LEFT: logic.left, Move.RIGHT: logic.right}
 
@@ -22,6 +25,7 @@ class GreedyMovePlayer(AbstractMovePlayer):
     the player receives time limit for a single step and the board as parameter and return the next move that gives
     the best score by looking one step ahead.
     """
+
     def get_move(self, board, time_limit) -> Move:
         optional_moves_score = {}
         for move in Move:
@@ -32,11 +36,95 @@ class GreedyMovePlayer(AbstractMovePlayer):
         return max(optional_moves_score, key=optional_moves_score.get)
 
 
+class HeuristicFunction:
+    def __init__(self):
+        self.w_empty = 2.7
+        self.w_max_val = 1
+        self.w_monotonic = 1
+        self.w_same_tiles = 1
+
+    def calc_heuristic(self, board):
+        empty = self.w_empty * self._empty_cells(board)
+        max_val = self.w_max_val * self._max_value(board)
+        mono_val = self.w_monotonic * self._monotonic_board(board)
+        same_tiles_val = self.w_same_tiles * self._same_tiles(board)
+
+        return empty + mono_val + max_val + same_tiles_val
+
+    def _empty_cells(self, board) -> float:
+        empty_cells = 0
+        for i in range(GRID_LEN):
+            for j in range(GRID_LEN):
+                if board[i][j] == 0:
+                    empty_cells += 1
+
+        if empty_cells == 0:
+            return 1
+        res = log(empty_cells)
+        return res
+
+    def _max_value(self, board) -> float:
+        max_val = 0
+        for i in range(GRID_LEN):
+            for j in range(GRID_LEN):
+                if board[i][j] > max_val:
+                    max_val = board[i][j]
+        return log(max_val)
+
+    def _monotonic_board(self, board):
+        left_to_right = 0
+        right_to_left = 0
+        up_to_down = 0
+        down_to_up = 0
+        val_curr = 0
+        val_next = 0
+        for x in range(GRID_LEN):
+            for y in range(GRID_LEN - 1):
+                # horizontal
+                val_curr = board[x][y]
+                val_next = board[x][y + 1]
+                if val_curr != 0 or val_next != 0:
+                    if val_curr >= val_next:
+                        left_to_right += 1
+                    if val_next >= val_curr:
+                        right_to_left += 1
+
+                    # vertical
+                val_curr = board[y][x]
+                val_next = board[y + 1][x]
+                if val_curr != 0 or val_next != 0:
+                    if val_curr >= val_next:
+                        up_to_down += 1
+                    if val_next >= val_curr:
+                        down_to_up += 1
+        res = max(right_to_left, left_to_right) + max(down_to_up, up_to_down)
+        if res == 0:
+            return 0
+        return log(res)
+
+    def _same_tiles(self, board):
+        counter = 0
+        for row in range(GRID_LEN):
+            for col in range(GRID_LEN - 1):
+                if board[row][col] == board[row][col + 1] and board[row][col] != 0:
+                    counter += 1
+                    col += 1
+        for col in range(GRID_LEN):
+            for row in range(GRID_LEN - 1):
+                if board[row][col] == board[row + 1][col] and board[row][col] != 0:
+                    counter += 1
+                    row += 1
+        if counter == 0:
+            return 0
+        return log(counter)
+
+
 class RandomIndexPlayer(AbstractIndexPlayer):
     """Random index player provided to you (no need to change),
     the player receives time limit for a single step and the board as parameter and return the next indices to
     put 2 randomly.
     """
+
     def get_indices(self, board, value, time_limit) -> (int, int):
         a = random.randint(0, len(board) - 1)
         b = random.randint(0, len(board) - 1)
@@ -52,15 +140,24 @@ class ImprovedGreedyMovePlayer(AbstractMovePlayer):
     implement get_move function with greedy move that looks only one step ahead with heuristic.
     (you can add helper functions as you want).
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
 
     def get_move(self, board, time_limit) -> Move:
         # TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        optional_moves_score = {}
+        heuristic = HeuristicFunction()
+        for move in Move:
+            new_board, done, score = commands[move](board)
+            if done:
+                optional_moves_score[move] = heuristic.calc_heuristic(new_board)
 
-    # TODO: add here helper functions in class, if needed
+        return max(optional_moves_score, key=optional_moves_score.get)
+
+
+# TODO: add here helper functions in class, if needed
 
 
 # part B
@@ -69,12 +166,14 @@ class MiniMaxMovePlayer(AbstractMovePlayer):
     implement get_move function according to MiniMax algorithm
     (you can add helper functions as you want).
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
 
     def get_move(self, board, time_limit) -> Move:
         # TODO: erase the following line and implement this function.
+
         raise NotImplementedError
 
     # TODO: add here helper functions in class, if needed
@@ -87,6 +186,7 @@ class MiniMaxIndexPlayer(AbstractIndexPlayer):
     implement get_indices function according to MiniMax algorithm, the value in minimax player value is only 2.
     (you can add helper functions as you want).
     """
+
     def __init__(self):
         AbstractIndexPlayer.__init__(self)
         # TODO: add here if needed
@@ -104,6 +204,7 @@ class ABMovePlayer(AbstractMovePlayer):
     implement get_move function according to Alpha Beta MiniMax algorithm
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -121,6 +222,7 @@ class ExpectimaxMovePlayer(AbstractMovePlayer):
     implement get_move function according to Expectimax algorithm.
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -137,6 +239,7 @@ class ExpectimaxIndexPlayer(AbstractIndexPlayer):
     implement get_indices function according to Expectimax algorithm, the value is number between {2,4}.
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractIndexPlayer.__init__(self)
         # TODO: add here if needed
@@ -154,6 +257,7 @@ class ContestMovePlayer(AbstractMovePlayer):
     implement get_move function as you want to compete in the Tournament
     (you can add helper functions as you want)
     """
+
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
@@ -163,4 +267,3 @@ class ContestMovePlayer(AbstractMovePlayer):
         raise NotImplementedError
 
     # TODO: add here helper functions in class, if needed
-
