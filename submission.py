08a7f0b8eb77efce1cd8da76cc39b4027e3f1aab
danibondar <danibondar@gmail.com>
2021-05-30 16:37:12 +0300

    part B - MinMax

--- submission.py
+++ submission.py
@@ -3,6 +3,7 @@ import random
 from AbstractPlayers import *
 from math import log
 import time
+import copy
 
 # commands to use for move players. dictionary : Move(enum) -> function(board),
 # all the functions {up,down,left,right) receive board as parameter and return tuple of (new_board, done, score).
@@ -38,8 +39,8 @@ class GreedyMovePlayer(AbstractMovePlayer):
 
 class HeuristicFunction:
     def __init__(self):
-        self.w_empty = 4
-        self.w_max_val = 1
+        self.w_empty = 3
+        self.w_max_val = 3
         self.w_monotonic = 1
         self.w_same_tiles = 2
 
@@ -48,7 +49,7 @@ class HeuristicFunction:
         max_val = self.w_max_val * self._max_value(board)
         mono_val = self.w_monotonic * self._monotonic_board_2(board)
         same_tiles_val = self.w_same_tiles * self._same_tiles(board)
-
+        # print(f'empty: {empty}, max_val: {max_val}, mono_val: {mono_val}, merge: {same_tiles_val}')
         return empty + mono_val + max_val + same_tiles_val
 
     def _empty_cells(self, board) -> float:
@@ -102,22 +103,30 @@ class HeuristicFunction:
 
     def _monotonic_board_2(self, board):
         left_to_right = 0
+        right_to_left = 0
         up_to_down = 0
+        down_to_up = 0
         for x in range(GRID_LEN):
             for y in range(GRID_LEN - 1):
                 # horizontal
                 val_curr = board[x][y]
                 val_next = board[x][y + 1]
-                if (val_curr != 0 or val_next != 0) and val_curr < val_next:
-                    left_to_right += val_next - val_curr
+                if val_curr != 0 or val_next != 0:
+                    if val_curr < val_next:
+                        left_to_right += val_next - val_curr
+                    if val_curr > val_next:
+                        right_to_left += val_curr - val_next
 
                 # vertical
                 val_curr = board[y][x]
                 val_next = board[y + 1][x]
-                if (val_curr != 0 or val_next != 0) and val_curr < val_next:
-                    up_to_down += val_next - val_curr
+                if val_curr != 0 or val_next != 0:
+                    if val_curr < val_next:
+                        up_to_down += val_next - val_curr
+                    if val_curr > val_next:
+                        down_to_up += val_curr - val_next
 
-        res = left_to_right + up_to_down
+        res = min(left_to_right, right_to_left) + min(up_to_down, down_to_up)
         if res == 0:
             return 0
         return -log(res)
@@ -163,10 +172,8 @@ class ImprovedGreedyMovePlayer(AbstractMovePlayer):
 
     def __init__(self):
         AbstractMovePlayer.__init__(self)
-        # TODO: add here if needed
 
     def get_move(self, board, time_limit) -> Move:
-        # TODO: erase the following line and implement this function.
         optional_moves_score = {}
         heuristic = HeuristicFunction()
         for move in Move:
@@ -175,9 +182,6 @@ class ImprovedGreedyMovePlayer(AbstractMovePlayer):
                 optional_moves_score[move] = heuristic.calc_heuristic(new_board)
 
         return max(optional_moves_score, key=optional_moves_score.get)
-
-
-# TODO: add here helper functions in class, if needed
 
 
 # part B
@@ -189,14 +193,47 @@ class MiniMaxMovePlayer(AbstractMovePlayer):
 
     def __init__(self):
         AbstractMovePlayer.__init__(self)
-        # TODO: add here if needed
 
     def get_move(self, board, time_limit) -> Move:
-        # TODO: erase the following line and implement this function.
+        move = Move.LEFT  # just to initialize
+        time_last_iteration = 0
+        iteration = 0
+        start = time.time()
+        while time_last_iteration * 16 < time_limit - (time.time() - start):
+        #while iteration < 2:  # debug
+            start_iteration = time.time()
+            move = self.min_max_move(board, iteration)[0]
+            time_last_iteration = time.time() - start_iteration
+            iteration += 1
+        print(f"move: {iteration}")
+        print(f"move last iter{time_last_iteration}")
+        print(f"move time left: {time_limit - (time.time() - start)}")
+        return move
 
-        raise NotImplementedError
-
-    # TODO: add here helper functions in class, if needed
+    def min_max_move(self, board, iteration) -> (Move, float):
+        optional_moves_score = {}
+        if iteration == 0:
+            heuristic = HeuristicFunction()
+            for move in Move:
+                new_board, done, score = commands[move](board)
+                if done:
+                    optional_moves_score[move] = heuristic.calc_heuristic(new_board)
+            if not optional_moves_score:
+                return Move.LEFT, 0  # need to put something in the second value
+            else:
+                res_move = max(optional_moves_score, key=optional_moves_score.get)
+                return res_move, optional_moves_score[res_move]
+        else:
+            index_player = MiniMaxIndexPlayer()
+            for move in Move:
+                new_board, done, score = commands[move](board)
+                if done:
+                    optional_moves_score[move] = index_player.min_max_index(new_board, iteration - 1)[1]
+            if not optional_moves_score:
+                return Move.LEFT, 0
+            else:
+                res_move = max(optional_moves_score, key=optional_moves_score.get)
+                return res_move, optional_moves_score[res_move]
 
 
 class MiniMaxIndexPlayer(AbstractIndexPlayer):
@@ -209,13 +246,45 @@ class MiniMaxIndexPlayer(AbstractIndexPlayer):
 
     def __init__(self):
         AbstractIndexPlayer.__init__(self)
-        # TODO: add here if needed
 
     def get_indices(self, board, value, time_limit) -> (int, int):
-        # TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        row = 0
+        col = 0
+        time_last_iteration = 0
+        iteration = 0
+        start = time.time()
+        while time_last_iteration * 16 < time_limit - (time.time() - start):
+        #while iteration < 2:  # debug
+            start_iteration = time.time()
+            row, col = self.min_max_index(board, iteration)[0]
+            time_last_iteration = time.time() - start_iteration
+            iteration += 1
+        print(f"index: {iteration}")
+        print(f"index time left: {time_limit - (time.time() - start)}")
 
-    # TODO: add here helper functions in class, if needed
+        return row, col
+
+    def min_max_index(self, board, iteration) -> ((int, int), float):
+        optional_index_score = {}
+        if iteration == 0:
+            heuristic = HeuristicFunction()
+            for row in range(GRID_LEN):
+                for col in range(GRID_LEN):
+                    if board[row][col] == 0:
+                        new_board = copy.deepcopy(board)
+                        new_board[row][col] = 2
+                        optional_index_score[(row, col)] = heuristic.calc_heuristic(new_board)
+            res_index = min(optional_index_score, key=optional_index_score.get)
+            return res_index, optional_index_score[res_index]
+        else:
+            move_player = MiniMaxMovePlayer()
+            for row in range(GRID_LEN):
+                for col in range(GRID_LEN):
+                    if board[row][col] == 0:
+                        new_board = copy.deepcopy(board)
+                        new_board[row][col] = 2
+                        optional_index_score[(row, col)] = (move_player.min_max_move(new_board, iteration - 1))[1]
+            return min(optional_index_score, key=optional_index_score.get), min(optional_index_score.values())
 
 
 # part C
